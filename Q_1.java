3a7fc0e5bb54b05f21f56e00a07ddaf878e75c6f
Abhishek0113 <95749290+Abhishek0113@users.noreply.github.com>
2024-03-21 12:10:52 +0530

    first commit

---
+++ Q_1.java
@@ -0,0 +1,18 @@
+package Exp_2;
+
+public class Q_1 {
+    public static void main(String[] args) {
+        int n1 = 7, n2 = 11, n3 = 30;
+        if(n1 >= n2) {
+            if(n1 >= n3)
+                System.out.println(n1 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        } else {
+            if(n2 >= n3)
+                System.out.println(n2 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        }
+    }
+}
