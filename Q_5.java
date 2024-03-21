c32396ce5e189c59b792856384761d475138e009
Abhishek0113 <95749290+Abhishek0113@users.noreply.github.com>
2024-03-21 12:34:00 +0530

    Hi.Hello

---
+++ Q_5.java
@@ -0,0 +1,13 @@
+package Exp_2;
+
+public class Q_5 {
+    public static void main(String args[]) {
+        int i, j, row=3;
+        for(i=0; i<row; i++) {
+            for(j=0; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
